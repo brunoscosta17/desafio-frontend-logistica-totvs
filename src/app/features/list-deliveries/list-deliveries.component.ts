c3fc8e2b2370ec781ebef5227d20d58168ce03a5
Bruno <brunoscosta17@gmail.com>
2024-07-21 01:30:57 -0300

    feat: Filtro por motorista e por status

--- src/app/features/list-deliveries/list-deliveries.component.ts
+++ src/app/features/list-deliveries/list-deliveries.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
 import { Delivery } from '../../models/Delivery.interface';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
@@ -11,10 +12,11 @@ import { DeliveryService } from '../../services/delivery.service';
   imports: [
     MatToolbarModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatInputModule
   ],
   templateUrl: './list-deliveries.component.html',
-  styleUrl: './list-deliveries.component.scss'
+  styleUrls: ['./list-deliveries.component.scss']
 })
 export class ListDeliveriesComponent implements OnInit, AfterViewInit {
 
@@ -23,19 +25,39 @@ export class ListDeliveriesComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  private filterValues: any = {
+    motorista: '',
+    status_entrega: ''
+  };
+
   constructor(private deliveryService: DeliveryService) { }
 
   ngOnInit(): void {
     this.getDeliveriesList();
+    this.dataSource.filterPredicate = this.createFilter();
   }
 
-
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
   }
 
   getDeliveriesList(): void {
     this.deliveryService.getDeliveries().subscribe(deliveries => this.dataSource.data = deliveries);
   }
 
+  applyFilter(event: Event, filterType: string): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.filterValues[filterType] = filterValue.trim().toLowerCase();
+    this.dataSource.filter = JSON.stringify(this.filterValues);
+  }
+
+  createFilter(): (data: Delivery, filter: string) => boolean {
+    return (data: Delivery, filter: string): boolean => {
+      const searchTerms = JSON.parse(filter);
+      return data.motorista.nome.toLowerCase().includes(searchTerms.motorista)
+          && data.status_entrega.toLowerCase().includes(searchTerms.status_entrega);
+    };
+  }
 }
