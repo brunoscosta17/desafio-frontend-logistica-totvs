c2a75f3129e8f2e58e514e85f3d819ff080310a4
Bruno <brunoscosta17@gmail.com>
2024-07-22 00:04:16 -0300

    Build static version for GitHub Pages

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-LH5WN3SO.mjs";import"./chunk-K5KAWM7A.mjs";import"./chunk-4EEC6DW7.mjs";import"./chunk-GBJTLWVX.mjs";import"./chunk-76RYLZ4T.mjs";import"./chunk-WKS5ROEK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
